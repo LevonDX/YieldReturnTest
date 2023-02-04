370a858e87e5ffc8f3f40fa0a69c5cf724a2e27b
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-02-04 12:11:05 +0400

    Add project files.

---
+++ YieldReturnTest/ProductCollection.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldReturnTest
+{
+    class ProductCollection : IEnumerable<Product>
+    {
+        private List<Product> products;
+
+        public ProductCollection()
+        {
+            this.products = new List<Product>();
+        }
+        public void Add(Product product)
+        {
+            this.products.Add(product);
+        }
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return new ProductCollectionEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class ProductCollectionEnumerator : IEnumerator<Product>
+        {
+            ProductCollection productCollection;
+            int currentIndex = -1;
+
+            public ProductCollectionEnumerator(ProductCollection products)
+            {
+                this.productCollection = products;
+            }
+            public Product Current
+            {
+                get
+                {
+                    return this.productCollection.products[currentIndex];
+                }
+            }
+
+            public bool MoveNext()
+            {
+                return ++currentIndex < this.productCollection.products.Count;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return this.productCollection.products[currentIndex];
+                }
+            }
+            public void Dispose()
+            {
+
+            }
+
+        }
+    }
+
+}
---
+++ YieldReturnTest/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace YieldReturnTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductCollection products = new ProductCollection();
+
+            products.Add(new Product() { Name = "Dzmeruk", Price = 100 });
+            products.Add(new Product() { Name = "Bred", Price = 500 });
+            products.Add(new Product() { Name = "Butter", Price = 1000 });
+            products.Add(new Product() { Name = "Cheese", Price = 300 });
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product);
+            }
+        }
+    }
+}
